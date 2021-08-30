667c67f71f768dd2a1b068a83411cf80f7ef30d2
ajaarni <70163146+ajaarni@users.noreply.github.com>
2021-08-29 20:51:38 -0400

    VS Code stoped working
    I don't know if it works because my vs code stopped working, and for some reason, my repo is different from my local so I am just manually uploading this.

--- strategy/BinarySearch.java
+++ strategy/BinarySearch.java
@@ -1,11 +1,54 @@
 package strategy;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class BinarySearch implements SearchBehavior{
     public boolean contains(ArrayList<String> data, String item){
-        return false;
+       /**
+        * https://www.geeksforgeeks.org/binary-search-a-string/
+        * used to base my algorithm
+        */
+        /**
+         * sorts the array list
+         */
+        Collections.sort(data);
+        /**
+         * finds the middle and sorts from there adjusting depending on 
+         * if it contians the desired word
+         */
+         int binarySearch(ArrayList<String> data, String item){
+             int left = 0;
+             int right = data.size()-1;
 
+             while(left <= right){
+                 int mid = left + (right - 1) / 2;
+                 int result = item.compareTo(data.get(mid));
+
+                 if (result == 0){
+                     return true;
+                 }
+                    
+                else if (result > 0){
+                    left = mid + 1;
+                }
+
+                else{
+                    right = mid - 1;
+                }
+                 
+             }
+             return false;
+
+         }
+        
     }
+
+
+        
+       
+    
+
+    
     
     
 }
--- strategy/LinearSearch.java
+++ strategy/LinearSearch.java
@@ -3,8 +3,22 @@ import java.util.ArrayList;
 
 public class LinearSearch implements SearchBehavior{
     public boolean contains(ArrayList<String> data, String item){
+       /**
+        * https://www.geeksforgeeks.org/linear-search/
+        used to base algorithm
+        */
+        int n = data.size();
+
+        for(int i = 0; i < n; i++){
+            if(data.get(i) == item)
+            {
+                return true;
+            }
+        }
         return false;
 
     }
+
+    
     
 }
