0dd2ffe7018de2836fd3bab47bc34b30db461b8c
Anton Aarnink <antonaarnink@MacBook-Pro-3.local>
2021-08-29 20:39:33 -0400

    VSCode stoped working

--- strategy/GuestList.java
+++ strategy/GuestList.java
@@ -1,3 +1,7 @@
+/**
+ * @author Anton Aarnink
+ * CSCE 247 002
+ */
 package strategy;
 import java.util.ArrayList;
 
@@ -5,29 +9,58 @@ public class GuestList{
     private String title;
     private ArrayList<String> people;
     private SearchBehavior searchBehavior;
-
+/**
+ * Setting the default search stratagy to Linear Search
+ */
     public GuestList(String title){
         searchBehavior = new LinearSearch();
-
-
     }
 
-    public boolean add(String person){
-        return false;
-
+    public Boolean add(String person){
+        if (searchBehavior.contains(people,person)){
+            /**
+             * @return sets the boolean value to false 
+             * triggering the appropriate response
+             */
+            return false;
+         }
+        else{
+            people.add(person);
+            /**
+             * @return sets the boolean value to true 
+             * triggering the appropriate response
+             */
+            return true;
+        }
     }
-
-    public boolean remove(String person){
-        return false;
-
-
+    /**
+     * class that checks the list for its sotred values 
+     * and removes is possible
+     */
+    public Boolean remove(String person){
+        if (searchBehavior.contains(people,person)){
+            people.remove(person);
+            /**
+             * @return sets the boolean value to true 
+             * meaning the person was on the list and it was removed
+             */
+            return true;
+         }
+        else{
+            //people.remove(person);
+            /**
+             * @return sets the boolean value to false 
+             * meaning the person was not on the list and couldn't be removed
+             */
+            return false;
+        }
     }
 
     public String getTitle(){
-        return "";
+        return this.title;
 
     }
-
+    
     public void setSearchBehavior(SearchBehavior searchBehavior){
         this.searchBehavior = searchBehavior;
 
--- strategy/SearchBehavior.java
+++ strategy/SearchBehavior.java
@@ -1,6 +1,8 @@
 package strategy;
 import java.util.ArrayList;
-
+/**
+ * requires a contains method in both the search methods 
+ */
 public interface SearchBehavior {
     public boolean contains(ArrayList<String> data, String item);
     
